4a3631204ea842ed096b85aa2880907b681d144b
unknown <mmhond1@students.towson.edu>
2021-11-21 00:44:05 -0500

    2nd

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send("You have hit root")
+});
+
+app.get('/pointOne', function (req, res) {
+
+    res.json({
+        person: "me",
+
+    });
+
+});
+
+var server = app.listen(8080, function () {
+    console.log('Server is running..');
+});
