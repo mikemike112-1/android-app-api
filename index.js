92e813efda0041f2e3afe852a0bcfad74e48ee7c
unknown <mmhond1@students.towson.edu>
2021-12-06 00:05:57 -0500

    added db

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const mysql = require('mysql');
+const credentials = require("./dbCred");
 var app = express();
 
 const port = process.env.PORT || 8080 ;
 
+var con = mysql.createConnection({
+    host: "androidclass.ctbicidjx9pg.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "password",
+    database: "androidClass"
+});
+
+
 app.get('/', function (req, res) {
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = "INSERT INTO testTable (colOne) VALUES ('infoStudd')";
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("1 record inserted");
+        });
+      });
+
     res.send("You have hit root. Use /foodBank for more!")
 });
+
+
 
 app.get('/foodBank', function (req, res) {
 
