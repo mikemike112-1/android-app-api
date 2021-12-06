bd6da9b1aebe593442c26800e8a65608f219b85a
unknown <mmhond1@students.towson.edu>
2021-12-05 23:30:37 -0500

    matching everything

--- index.js
+++ index.js
@@ -4,15 +4,35 @@ var app = express();
 const port = process.env.PORT || 8080 ;
 
 app.get('/', function (req, res) {
-    res.send("You have hit root. Use /currentLocation for more!")
+    res.send("You have hit root. Use /foodBank for more!")
 });
 
-app.get('/currentLocation', function (req, res) {
+app.get('/foodBank', function (req, res) {
 
     res.json({
-        city: "Towson", 
-        name: "Buddy's Treats"
+        'locationOne' : [{
+            city: "Towson", 
+            name: "Nova's Creations"
+        }],
+        'locationTwo' : [{
+            city: "Towson", 
+            name: "Pumpkin's Creations"
+        }]
+    });
 
+});
+
+app.get('/restaurants', function (req, res) {
+
+    res.json({
+        'locationOne' : [{
+            city: "Towson", 
+            name: "Buddy's Treats"
+        }],
+        'locationTwo' : [{
+            city: "Towson", 
+            name: "Syd's Restaurant"
+        }]
     });
 
 });
