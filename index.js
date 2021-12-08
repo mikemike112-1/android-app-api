b63f5ebd53cf9c3f86ec83c6b82d96ef9bc044d0
unknown <mmhond1@students.towson.edu>
2021-12-08 00:28:14 -0500

    simply json

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const mysql = require('mysql');
-const credentials = require("./dbCred");
+//const mysql = require('mysql');
+//const credentials = require("./dbCred");
 var app = express();
 
 const port = process.env.PORT || 8080 ;
-
+/*
 var con = mysql.createConnection({
     host: "androidclass.ctbicidjx9pg.us-east-2.rds.amazonaws.com",
     user: "admin",
     password: "password",
     database: "androidClass"
 });
-
+*/
 
 app.get('/', function (req, res) {
 
-    con.connect(function(err) {
+    /*con.connect(function(err) {
         if (err) throw err;
         console.log("Connected!");
         var sql = "INSERT INTO testTable (colOne) VALUES ('infoStudd')";
@@ -24,7 +24,7 @@ app.get('/', function (req, res) {
           console.log("1 record inserted");
         });
       });
-
+*/
     res.send("You have hit root. Use /foodBank for more!")
 });
 
@@ -60,6 +60,7 @@ app.get('/restaurants', function (req, res) {
 
 });
 
-var server = app.listen(port, function () {
+//var server = 
+app.listen(port, function () {
     console.log('Server is running..');
 });
