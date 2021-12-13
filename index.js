49984804531ae5f82739978828b2ff7ae2dd3fe5
unknown <mmhond1@students.towson.edu>
2021-12-12 22:41:05 -0500

    combined everything

--- index.js
+++ index.js
@@ -1,68 +1,136 @@
+/*
 const express = require('express');
 //const mysql = require('mysql');
 //const credentials = require("./dbCred");
 var app = express();
 
 const port = process.env.PORT || 8080 ;
-/*
+*/
+
+const express = require('express');
+const mysql = require('mysql');
+const credentials = require("./dbCred");
+var app = express();
+
+const port = process.env.PORT || 8080 ;
+
 var con = mysql.createConnection({
     host: "androidclass.ctbicidjx9pg.us-east-2.rds.amazonaws.com",
     user: "admin",
     password: "password",
-    database: "androidClass"
+    database: "editables"
 });
-*/
 
+con.connect(function(err) {
+    if(err) throw err;
+});
+
+//www.yoursite.com?myparam1={id1}&myparam2={id2} 
+//http://localhost:8080/?myparam1={id1}&myparam2={id2}
+//http://localhost:8080/?idP=v1&nameP=v2&locationP=v3&distanceP=v4&picURLP=v5&isAcceptingP=v6
 app.get('/', function (req, res) {
 
-    res.send("You have hit root. Use... /foodBanks /restaurants")
-});
+    var idQ = req.query.idP;
+    var nameQ = req.query.nameP;
+    var locationQ = req.query.locationP;
+    var distanceQ = req.query.distanceP;
+    var picURLQ = req.query.picURLP;
+    var isAcceptingQ = req.query.isAcceptingP;
 
+    //url parameters format is...
+    //?idP={idQ}
+
+    console.log(req.query);
+    console.log(req.query.myparam1); 
+
+    //"INSERT INTO restaurants (id, name, location, distance, picURL, isAccepting) VALUES ('a', 'b', 'c', 'd', 'e', 'f')";
+    //var restaurantQuery = "UPDATE restaurants SET id = 'idB', name = 'nameA', location = 'locationA', distance = 'distanceA', picURL = 'picURLA', isAccepting = 'isAcceptingA'";
+
+    //STOPPED HERE 
+    var restaurantQuery 
+    = "UPDATE restaurants SET " 
+    + "id = '" 
+    + idQ 
+    + "', name = '"
+    + nameQ  
+    + "', location = '"
+    + locationQ
+    + "', distance = '"
+    + distanceQ
+    + "', picURL = '" 
+    + picURLQ
+    + "', isAccepting = '"
+    + isAcceptingQ
+    + "'";
+
+    con.query(restaurantQuery, function (err, result, fields){
+            if(err) throw err;
+            console.log(result);
+            res.send(result);
+    });
+
+});
 
 
 app.get('/foodBanks', function (req, res) {
 
-    res.json({
-        'locationOne' : [{
+    res.json(
+        {
+            id: 0,
+            name: "Please update",
+            location: "Update", 
+            distance: 5,
+            picUrl: "update",
+            isAccepting: true
+        },        
+        {
             id: 1,
             name: "Buddy's Creations",
             location: "Towson", 
             distance: 5,
             picUrl: "https://dogtime.com/assets/uploads/gallery/jack-russel-terrier-dog-breed-pictures/2-face.jpg",
             isAccepting: true
-        }],
-        'locationTwo' : [{
+        },
+        {
             id: 2,
             name: "Nacho Mama's",
             location: "Towson", 
             distance: 10,
             picUrl: "https://nachomamasmd.com/wp-content/uploads/2017/08/nacho-mamas-logo.png",
             isAccepting: true
-        }]
-    });
+        }
+    );
 
 });
 
 app.get('/restaurants', function (req, res) {
 
-    res.json({
-        'locationOne' : [{
+    res.json(
+        {
+            id: 0,
+            name: "Please update",
+            location: "Update", 
+            distance: 5,
+            picUrl: "update",
+            isAccepting: true
+        },     
+        {
             id: 1,
             name: "Buddy's Creations",
             location: "Towson", 
             distance: 5,
             picUrl: "https://dogtime.com/assets/uploads/gallery/jack-russel-terrier-dog-breed-pictures/2-face.jpg",
             isAccepting: true
-        }],
-        'locationTwo' : [{
+        },
+        {
             id: 2,
             name: "Nacho Mama's",
             location: "Towson", 
             distance: 10,
             picUrl: "https://nachomamasmd.com/wp-content/uploads/2017/08/nacho-mamas-logo.png",
             isAccepting: true
-        }]
-    });
+        }
+    );
 });
 
 //var server = 
